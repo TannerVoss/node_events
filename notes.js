54a1ddbb743e0650900863d6be7c47716334cf91
Tanner <TannerVoss1231@gmail.com>
2021-12-05 18:40:57 -0700

    submit

--- notes.js
+++ notes.js
@@ -1,8 +1,9 @@
+// Complete Events Exercise
+
 const { EventEmitter } = require("events"); //Import “EventEmitter” from the events module
 const { createServer } = require("http");
 const { appendFile, readFile, createReadStream, read } = require("fs");
 const path = require("path");
-
 
 const NewsLetter = new EventEmitter(); // Using the EventEmitter class, create a new EventEmitter instance called “NewsLetter”
 
@@ -10,6 +11,9 @@ const server = createServer((req, res) => { //Instantiate a new server instance 
     const { url, method } = req;
 
     req.on("error", (err) => { // Account for errors
+
+
+
         console.log(err); // Listen for error events on the request and response objects, and send back appropriate status codes and messages.
         res.statusCode = 400;
         res.setHeader("Content-Type", "application/json");
@@ -17,40 +21,32 @@ const server = createServer((req, res) => { //Instantiate a new server instance 
         res.end();
     });
 
-    res.on("error", (err) => {
-        console.log(err); // Listen for error events on the request and response objects, and send back appropriate status codes and messages.
+    res.on("error", (err) => { // Listen for error events on the request and response objects, and send back appropriate status codes and messages.
+        console.log(err);
         res.statusCode = 500;
         res.setHeader("Content-Type", "application/json");
         res.write(JSON.stringify({ msg: "Server error" }));
         res.end();
     });
 
-    //   req.pipe(res);
 
     const chunks = []; // pass chunks into an array
 
     req.on("data", (chunk) => { //Inside the server request handler, listen for the ‘data’ event to be emitted
         chunks.push(chunk); // and pass in a callback function that pushes each ‘chunk’ into an array named ‘chunks’
     });
+
     req.on("end", () => { //Listen for the request ReadStream ‘end’ event
         if (url === "/newsletter_signup" && method === "POST") { // check the request url and method, and if they are “POST” and “/newsletter_signup”, 
             const body = JSON.parse(Buffer.concat(chunks).toString()); // decode the chunks array with Buffer.concat().toString(), and use JSON.parse() on the result to access the request body values
+
             const newContact = `${body.name},${body.email}\n`; // emit a ‘signup’ event, and pass in the string value to be used by your event listener callback (name + email for csv record)
             NewsLetter.emit("signup", newContact, res); // fire event "NewsLetter"
+
             res.setHeader("Content-Type", "application/json");
-            res.write(
-                JSON.stringify({ msg: "Signed up" }) // write response to the client
-            );
+            res.write(JSON.stringify({ msg: "Successfully signed up for newsletter" })); // write response to the client
             res.end(); // end response
 
-            // A get request to your “/newsletter_signup” endpoint should send back an html page with a form
-        } else if (url === "/newsletter_signup" && method === "GET") { //Add an else/if to your conditional that checks for request method and url.
-
-            res.setHeader("Content-Type", "text/html");
-            const readStream = createReadStream(
-                path.join(__dirname, "./public/index.html")
-            );
-            readStream.pipe(res);
         } else { // Account for any method or url that is not “POST” + “/newsletter_signup”
             res.statusCode = 404;
             res.setHeader("Content-Type", "application/json");
@@ -58,30 +54,27 @@ const server = createServer((req, res) => { //Instantiate a new server instance 
             res.end();
         }
     });
-});
 
-server.listen(3000, () => console.log("Server listening..."));
-
-NewsLetter.on("signup", (newContact, res) => { // Outside of your server request handler, add an event listener for a ‘signup’ event on the NewsLetter EventEmitter. Pass in a callback that takes in a ‘contact’ as a parameter
-    appendFile( // This function should use fs.appendFile() to  
-        path.join(__dirname, "./assets/newsletter.csv"), // add the contact to a csv file in your project directory
-        newContact,
-        (err) => {
-            if (err) {
-                NewsLetter.emit("error", err, res);
-                return;
+    NewsLetter.on("signup", (newContact, res) => { // Outside of your server request handler, add an event listener for a ‘signup’ event on the NewsLetter EventEmitter. Pass in a callback that takes in a ‘contact’ as a parameter
+        appendFile( // This function should use fs.appendFile() to 
+            path.join(__dirname, "./assets/newsletter.csv"), // add the contact to a csv file in your project directory
+            newContact, (err) => {
+                if (err) {
+                    NewsLetter.emit("error", err, res);
+                    return;
+                };
+                console.log("The file was updated successfully!");
             }
-            console.log("File updated");
-        }
-    );
-});
+        );
+    });
 
-NewsLetter.on("error", (err, res) => { // Listen for an error event on the NewLetter emitter, and 
-    console.log(err);
-    res.statusCode = 500; // send back a 500 server error status code 
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify({ msg: "Error adding new contact to newsletter" })); // with an appropriate response message.
-    res.end();
-});
+    NewsLetter.on("error", (err, res) => { // Listen for an error event on the NewLetter emitter, and 
 
-//
+        console.log(err);
+        res.statusCode = 500; // send back a 500 server error status code 
+        res.setHeader("Content-Type", "application/json");
+        res.write(JSON.stringify({ msg: "Error adding new contact to newsletter" })); // with an appropriate response message.
+        res.end();
+    });
+
+});
